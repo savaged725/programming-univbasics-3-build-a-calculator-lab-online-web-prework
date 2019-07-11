8586741784d47b0edc1c524340b3b55d107b2171
Deborah Savage <savaged725@yahoo.com>
2019-07-11 17:07:24 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,32 @@
 def addition(num1, num2)
-
-end
+  puts 5 + 4
+  return 9
+  end
 
 def subtraction(num1, num2)
-
+  puts 10 - 5
+  return 5
 end
 
 def division(num1, num2)
+  puts 50 / 2
+  return 25
 
 end
 
 def multiplication(num1, num2)
-
-end
+  puts 4 * 30
+  return 120
+  end
 
 def modulo(num1, num2)
+  puts 34 % 5
+  return 4
 
 end
 
 def square_root(num)
+  puts 81 **
+  return 9
 
 end
